449716972deea6cbce5807f1512db858583d5b5b
swerty <slw247@cornell.edu>
2014-01-30 21:47:32 -0500

    first commit

---
+++ Example App/Forecastr/FCPlaylistTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FCPlaylistTableViewController.h
+//  Forecastr
+//
+//  Created by Sean Wertheim on 1/30/14.
+//  Copyright (c) 2014 Rob Phillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCPlaylistTableViewController : UITableViewController
+
+@end
